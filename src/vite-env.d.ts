859c068905f481c0c7c4549b0d808e7d384cabfe
zenlai2020 <69424162+zenlai2020@users.noreply.github.com>
2026-03-24 09:50:46 +0800

    修复无法部署的bug

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,15 +1,15 @@
 /// <reference types="vite/client" />
 
-interface WeixinJSBridge {
-  invoke(
-    api: string,
-    params: Record<string, unknown>,
-    callback: (res: unknown) => void
-  ): void
-}
-
 declare global {
   interface Window {
-    WeixinJSBridge?: WeixinJSBridge
+    WeixinJSBridge?: {
+      invoke(
+        api: string,
+        params: Record<string, unknown>,
+        callback: (res: unknown) => void
+      ): void
+    }
   }
 }
+
+export {}
